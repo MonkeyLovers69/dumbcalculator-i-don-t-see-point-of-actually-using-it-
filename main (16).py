a29d6e34ee94c9fd50ae5b267b6c663f5cc48b2c
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2023-07-09 00:09:18 +0500

    Add files via upload

---
+++ main (16).py
@@ -0,0 +1,12 @@
+print('hello i am easy made calculator for pretty dumb operations')
+a = float(input())
+operation = input()
+b = float(input())
+if operation == "*":
+    print(a * b)
+elif operation == '/':
+    print(a / b)
+elif operation == '+':
+    print(a + b)
+elif operation == '-':
+    print(a - b)
